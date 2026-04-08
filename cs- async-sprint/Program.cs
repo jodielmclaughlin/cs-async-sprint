301a34f305c3854867e2e40ddf2e2d98d43326c3
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-04-08 14:32:09 +0100

    Completed task 5

--- cs- async-sprint/Program.cs
+++ cs- async-sprint/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq.Expressions;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Numerics;
 
 namespace cs__async_sprint
 {
@@ -7,43 +9,63 @@ namespace cs__async_sprint
     {
         static async Task Main(string[] args)
         {
-            
-            Random rnd = new Random();
-            CancellationTokenSource source = new CancellationTokenSource();
-            CancellationToken token = source.Token;
-            
-            //Stopwatch stopwatch = Stopwatch.StartNew();
 
-            Task<string> task1 = Task.Run(async () =>
+            string data = "85671 34262 92143 50984 24515 68356 77247 12348 56789 98760";
+            List<BigInteger> stringToBigInteger = data.Split(' ').Select(x => BigInteger.Parse(x)).ToList();
+
+            List<Task<BigInteger>> tasks = stringToBigInteger.Select(biginteger => Task.Run(() => Exercises.CalculateFactorial(biginteger))).ToList();
+            var results = await Task.WhenAll(tasks);
+            foreach (var result in results)
             {
-                //stopwatch.Start();
-                await Task.Delay(rnd.Next(1000, 10000),token);
-                return "Hello...";
-            });
-
-            Task<string> task2 = Task.Run(async () =>
-            {
-                await Task.Delay(rnd.Next(1000, 10000),token);
-                return "...World!";
-                //stopwatch.Stop();
-            });
-
-
-            try
-            {
-                source.CancelAfter(5000);
-                var combinedTask = await Task.WhenAll(task1, task2);
-                Console.WriteLine(combinedTask[0] + combinedTask[1]);
-                
+                Console.WriteLine(result);
             }
-            catch (OperationCanceledException ex) 
-            {
-                Console.WriteLine($"{ex.Message}");
-            }
-            
-            
-                
-               
+            //Approach with ContinueWith included:
+            //var tasks = data.Split(' ')
+            //    .Select(x => BigInteger.Parse(x))
+            //    .Select(biginteger => Task.Run(() => Exercises.CalculateFactorial(biginteger)))
+            //    .Select(task => task.ContinueWith(t => Console.WriteLine(t.Result))).ToList();
+            //await Task.WhenAll(tasks);
+       
+
+
+
+
+            //Random rnd = new Random();
+            //CancellationTokenSource source = new CancellationTokenSource();
+            //CancellationToken token = source.Token;
+
+            ////Stopwatch stopwatch = Stopwatch.StartNew();
+
+            //Task<string> task1 = Task.Run(async () =>
+            //{
+            //    //stopwatch.Start();
+            //    await Task.Delay(rnd.Next(1000, 10000),token);
+            //    return "Hello...";
+            //});
+
+            //Task<string> task2 = Task.Run(async () =>
+            //{
+            //    await Task.Delay(rnd.Next(1000, 10000),token);
+            //    return "...World!";
+            //    //stopwatch.Stop();
+            //});
+
+
+            //try
+            //{
+            //    source.CancelAfter(5000);
+            //    var combinedTask = await Task.WhenAll(task1, task2);
+            //    Console.WriteLine(combinedTask[0] + combinedTask[1]);
+
+            //}
+            //catch (OperationCanceledException ex) 
+            //{
+            //    Console.WriteLine($"{ex.Message}");
+            //}
+
+
+
+
 
         }
     }

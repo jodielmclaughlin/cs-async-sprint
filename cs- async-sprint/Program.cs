32105f57c136dc540b870c8d6c030e5779af9e3a
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-04-08 10:51:53 +0100

    Completed second task

--- cs- async-sprint/Program.cs
+++ cs- async-sprint/Program.cs
@@ -4,12 +4,26 @@
     {
         static async Task Main(string[] args)
         {
-            await DelayPrint();
-            static async Task DelayPrint()
+            //await DelayPrint();
+            //static async Task DelayPrint()
+            //{
+            //    await Task.Delay(2000);
+            //    Console.WriteLine("Hello, World!");
+            //}
+
+            static async Task DelayHello()
             {
-                await Task.Delay(2000);
-                Console.WriteLine("Hello, World!");
+                await Task.Delay(3000);
+                Console.WriteLine("Hello");
             }
+
+            static async Task DelayWorld()
+            {
+                await Task.Delay(3000);
+                Console.WriteLine("World!");
+            }
+            await Task.WhenAll(DelayHello(), DelayWorld());            
+
         }
     }
 }

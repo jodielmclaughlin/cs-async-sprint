5cb2ce93da458da42e6133ca7d35778ccc9dd99e
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-04-08 10:43:20 +0100

    Created the repo and completed task 1

---
+++ cs- async-sprint/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace cs__async_sprint
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await DelayPrint();
+            static async Task DelayPrint()
+            {
+                await Task.Delay(2000);
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
